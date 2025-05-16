dcc908995232892624aa3f3a3dac69ae11ae6488
YatoCrow <yatocrowgames@gmail.com>
2025-05-16 09:54:18 +0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     <div className="home-container">
       <div className="hero">
         <img
-          src="../assets/logo.png"
+          src="/assets/chronoshift-logo.png"
           alt="Chronoshift"
           className="hero-logo"
         />
