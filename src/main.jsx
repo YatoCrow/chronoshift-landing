d146937c697cfe2cf9b034270d8b099bec7848fe
YatoCrow <yatocrowgames@gmail.com>
2025-05-16 09:21:39 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/faction' element={<div>Factions Page</div>} />
-        <Route path='/starter-decks' element={<div>Starter Decks Page</div>} />
-        <Route path='/rules' element={<div>Rules Page</div>} />
-        <Route path='/keywords' element={<div>Keywords Page</div>} />
-        <Route path='/tracker' element={<div>Tracker Page</div>} />
+        <Route path="/*" element={<App />} />
       </Routes>
     </Router>
   </React.StrictMode>
