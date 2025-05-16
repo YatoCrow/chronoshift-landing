ef1e66921c98130d1b8ae1f6336721ad216f8722
YatoCrow <yatocrowgames@gmail.com>
2025-05-16 11:47:08 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,12 @@ function App() {
       
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/factions" element={<h2>Factions Page</h2>} />
+        <Route path="/factions" element={<Factions />} />
         <Route path="/starter-decks" element={<h2>Starter Decks Page</h2>} />
         <Route path="/rules" element={<h2>Rules Page</h2>} />
         <Route path="/keywords" element={<h2>Keyword Glossary</h2>} />
         <Route path="/terms" element={<TermsOfUse />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
-        <Route path="/factions" element={<Factions />} />
 
         {/* Catch-all 404 route */}
         <Route
