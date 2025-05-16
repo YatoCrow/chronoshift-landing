df1a84003936a969965e1fe41fe73d8962fa54d5
YatoCrow <yatocrowgames@gmail.com>
2025-05-16 11:10:57 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/factions" element={<h2>Factions Page</h2>} />
@@ -13,6 +15,8 @@ function App() {
         <Route path="/rules" element={<h2>Rules Page</h2>} />
         <Route path="/keywords" element={<h2>Keyword Glossary</h2>} />
       </Routes>
+
+      <Footer />
     </>
   );
 }
