2b83be050a3b1dae095943f425ca48d3c825e427
YatoCrow <yatocrowgames@gmail.com>
2025-05-16 09:19:05 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,8 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div
-      style={{
-        maxWidth: "640px",
-        width: "100%",
-        margin: "0 auto",
-        padding: "2rem",
-        boxSizing: "border-box",
-      }}
-    >
+    <>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/factions" element={<h2>Factions Page</h2>} />
@@ -22,7 +13,7 @@ function App() {
         <Route path="/rules" element={<h2>Rules Page</h2>} />
         <Route path="/keywords" element={<h2>Keyword Glossary</h2>} />
       </Routes>
-    </div>
+    </>
   );
 }
 
