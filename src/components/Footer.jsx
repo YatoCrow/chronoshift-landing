ed8bb3d1390dd60a9d8c943b4100c2c9f9e34e75
YatoCrow <yatocrowgames@gmail.com>
2025-05-18 03:54:26 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,19 +8,21 @@ function Footer() {
       <div className="footer-content">
 
         <div className="footer-links">
-  <Link to="/">Home</Link>
-  <Link to="/factions">Factions</Link>
-  <Link to="/starter-decks">Starter Decks</Link>
-  <Link to="/rules">Rules</Link>
-  <Link to="/keywords">Keywords</Link>
-  <a href="https://chronoshift-tracker.yatocrowgames.com" target="_blank" rel="noreferrer">Tracker</a>
-  <Link to="/terms">Terms of Use</Link>
-  <Link to="/privacy">Privacy Policy</Link>
-</div>
+          <Link to="/">Home</Link>
+          <Link to="/factions">Factions</Link>
+          <Link to="/starter-decks">Starter Decks</Link>
+          <Link to="/rules">Rules</Link>
+          <Link to="/keywords">Keywords</Link>
+          <a href="https://chronoshift-tracker.yatocrowgames.com" target="_blank" rel="noreferrer">Tracker</a>
+          <Link to="/terms">Terms of Use</Link>
+          <Link to="/privacy">Privacy Policy</Link>
+        </div>
 
         <div className="footer-bottom">
           <p>© {new Date().getFullYear()} YatoCrow Games. All rights reserved.</p>
-          <p><em>Chronoshift: Eclipse Dawn™</em> are trademarks of YatoCrow Games.</p>
+          <p>
+            <em>Chronoshift™</em> and <em>Eclipse Dawn™</em> are trademarks of YatoCrow Games.
+          </p>
         </div>
 
       </div>
