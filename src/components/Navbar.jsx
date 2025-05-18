2216c64fd83a723896f0f24370e5dd31682f3fe5
YatoCrow <yatocrowgames@gmail.com>
2025-05-18 21:46:44 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,38 @@
 // src/components/Navbar.jsx
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import logo from "../assets/chronoshift-logo.png"; // Use your actual logo file path
+import logo from "../assets/chronoshift-logo.png";
 
 function Navbar() {
-  return (
-    <div className="navbar-wrapper">
-      <Link to="/" className="navbar-logo">
-        <img src={logo} alt="Chronoshift Logo" />
-      </Link>
+  const [open, setOpen] = useState(false);
 
-      <div className="navbar-links">
-        <Link to="/factions">Factions</Link>
-        <Link to="/starter-decks">Starter Decks</Link>
-        <Link to="/rules">Rules</Link>
-        <Link to="/keywords">Keywords</Link>
-        <a href="https://chronoshift-tracker.yatocrowgames.com" target="_blank" rel="noreferrer">Tracker</a>
+  return (
+    <nav className="navbar-wrapper">
+      <div className="navbar-left">
+        <Link to="/" className="navbar-logo">
+          <img src={logo} alt="Chronoshift Logo" />
+        </Link>
       </div>
-    </div>
+
+      <button
+        className={`burger ${open ? "open" : ""}`}
+        onClick={() => setOpen(!open)}
+        aria-label="Toggle menu"
+      >
+        <span className="burger-bar" />
+        <span className="burger-bar" />
+        <span className="burger-bar" />
+      </button>
+
+      <div className={`navbar-links ${open ? "open" : ""}`}>
+        <Link to="/factions" onClick={() => setOpen(false)}>Factions</Link>
+        <Link to="/starter-decks" onClick={() => setOpen(false)}>Starter Decks</Link>
+        <Link to="/rules" onClick={() => setOpen(false)}>Rules</Link>
+        <Link to="/keywords" onClick={() => setOpen(false)}>Keywords</Link>
+        <a href="https://chronoshift-tracker.yatocrowgames.com" target="_blank" rel="noreferrer" onClick={() => setOpen(false)}>Tracker</a>
+      </div>
+    </nav>
   );
 }
 
