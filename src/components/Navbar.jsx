846c95df2ed0a2d0fd10b14186c70e8fc5cc931c
YatoCrow <yatocrowgames@gmail.com>
2025-05-21 15:07:43 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 // src/components/Navbar.jsx
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../assets/chronoshift-logo.png";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
+  const location = useLocation();
+
+  // Close menu on route change
+  const handleClick = () => setOpen(false);
 
   return (
     <nav className="navbar-wrapper">
       <div className="navbar-left">
-        <Link to="/" className="navbar-logo">
+        <Link to="/" className="navbar-logo" onClick={handleClick}>
           <img src={logo} alt="Chronoshift Logo" />
         </Link>
       </div>
@@ -26,11 +30,11 @@ function Navbar() {
       </button>
 
       <div className={`navbar-links ${open ? "open" : ""}`}>
-        <Link to="/factions" onClick={() => setOpen(false)}>Factions</Link>
-        <Link to="/starter-decks" onClick={() => setOpen(false)}>Starter Decks</Link>
-        <Link to="/rules" onClick={() => setOpen(false)}>Rules</Link>
-        <Link to="/keywords" onClick={() => setOpen(false)}>Keywords</Link>
-        <a href="https://chronoshift-tracker.yatocrowgames.com" target="_blank" rel="noreferrer" onClick={() => setOpen(false)}>Tracker</a>
+        <Link to="/factions" onClick={handleClick}>Factions</Link>
+        <Link to="/starter-decks" onClick={handleClick}>Starter Decks</Link>
+        <Link to="/rules" onClick={handleClick}>Rules</Link>
+        <Link to="/keywords" onClick={handleClick}>Keywords</Link>
+        <Link to="/tracker" onClick={handleClick}>Tracker</Link>
       </div>
     </nav>
   );
