acd2f100c8fe069fc0981e9b34c94447d2287d5f
YatoCrow <yatocrowgames@gmail.com>
2025-05-22 17:03:28 +0800

    Create ChronologLore.jsx

---
+++ src/pages/ChronologLore.jsx
@@ -0,0 +1,6 @@
+import chronologEntries from "../data/loreData";
+import Chronolog from "./Chronolog";
+
+export default function ChronologLore() {
+  return <Chronolog title="Lore" entries={chronologEntries} />;
+}
