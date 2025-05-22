2bafc78cc0f681ee2201ce3164b4e14d4067acb6
YatoCrow <yatocrowgames@gmail.com>
2025-05-22 17:06:22 +0800

    Update ChronologNews.jsx

--- src/pages/ChronologNews.jsx
+++ src/pages/ChronologNews.jsx
@@ -1,5 +1,5 @@
 import chronologEntries from "../data/newsData";
-import Chronolog from "./Chronolog";
+import Chronolog from "../components/Chronolog"; // ✅ Correct path
 
 export default function ChronologNews() {
   return <Chronolog title="News & Updates" entries={chronologEntries} />;
